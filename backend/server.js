893fc302c1868f4f37ed3ca33f527fd2008efb32
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2025-06-21 16:26:42 +0530

    feat: added web sockets to the application using context api on client

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,19 @@ app.use(bodyParser.json());
 
 const io = new Server()
 
+const emailToSocket = new Map();
+
+io.on('connection', (socket) => {
+    console.log('new connection')
+    socket.on('join-room', (data) => {
+        const {roomId, emailId} = data;
+        console.log("user ",emailId, " joined room no : ",roomId);
+        emailToSocket.set(emailId, socket.id);
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit("user-joined", {emailId});
+    })
+})
+
 app.listen(PORT, () => {
     console.log('The app is listening on port ', PORT);
 })  
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import Homepage from './pages/Homepage'
-
+import { SocketProvider } from './providers/Socket'
 
 function App() {
   return (
     <Routes>
+      <SocketProvider>
       <Route path='/' element={<Homepage/>} />
       <Route path='/add' element = {<h1>add</h1>}/>
+      </SocketProvider>
     </Routes>
    
     
