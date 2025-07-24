4bc84450d3c7309714bc68eb6226cf679d7778e1
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2025-07-24 22:26:25 +0530

    fix : restart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,9 @@
-import {Routes, Route} from 'react-router-dom'
-import Homepage from './pages/Homepage'
-import { SocketProvider } from './providers/Socket'
-
 function App() {
   return (
-    <Routes>
-      <SocketProvider>
-      <Route path='/' element={<Homepage/>} />
-      <Route path='/add' element = {<h1>add</h1>}/>
-      </SocketProvider>
-    </Routes>
-   
-    
+ <div>
+  Home
+ </div> 
   )
-  
 }
 
 export default App
