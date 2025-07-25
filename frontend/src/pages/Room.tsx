5356db4dbcad248d3e905ffb1b48ada26a0839d5
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2025-07-25 19:38:30 +0530

    feat: handled rtc connection set up and sending the incoming call to reciever

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { useSocket } from '../contexts/SocketProvider'
-import ReactPlayer from 'react-player'
 import RoomVideo from '../components/RoomVideo';
+import peer from '../service/peer'
+
+
 interface Idata{
   email : string;
   id : string;
@@ -10,26 +12,67 @@ interface Idata{
 function Room() {
     const [connectedSocket, setConnectedSocket] = useState<string | null>(null);
     const [myStream, setMyStream] = useState<MediaStream | null>(null);
+    const [remoteStream, setRemoteStream] = useState<any>(null);
     const socket = useSocket();
 
     const handleUserJoined = useCallback(({email, id}:Idata) => {
         console.log(`Email user ${email} joined room ${id}`)
         setConnectedSocket(id);
     },[])
+
+
     const handleCallUser = useCallback(async () => {
         const stream = await navigator.mediaDevices.getUserMedia({
             audio:true,
             video: true
         })
+        const offer = await peer.getOffer();
+        socket.emit("user:call", {to:connectedSocket, offer})
         setMyStream(stream);
+    }, [socket, connectedSocket])
+
+    const handleIncomingCall = useCallback(async ({from, offer}:any) => {
+      console.log('incoming call ', from, offer);
+         setConnectedSocket(from)
+         const stream = await navigator.mediaDevices.getUserMedia({
+            audio:true,
+            video: true
+        })
+        setMyStream(stream);
+      const ans = await peer.getAnswer(offer);
+      socket.emit('call:accepted',{to:from, ans});
+    },[socket])
+
+    const handleCallAccepted = useCallback(async ({from, ans}:any) => {
+          if (!myStream) return;
+    if (!peer.peer) return;
+     await peer.setLocalDescription(ans);
+     for(const track of myStream?.getTracks()){
+        peer.peer?.addTrack(track, myStream);
+     }
+      console.log('call accepted', from, ans);
+    } ,[myStream])
+
+
+    useEffect(() => {
+      peer.peer?.addEventListener('track', async ev => {
+        const remoteStream = ev.streams
+        setRemoteStream(remoteStream);
+      })
     }, [])
+
 useEffect(() => {
     socket.on("user:joined", handleUserJoined)
-
+    socket.on('incoming:call', handleIncomingCall);
+    socket.on('call:accepted', handleCallAccepted);
     return () => {
         socket.off('user:joined', handleUserJoined)
+        socket.off('incoming:call', handleIncomingCall);
+        socket.off('call:accepted', handleCallAccepted);
+
     }
-}, [socket, handleUserJoined])
+}, [socket, handleUserJoined, handleIncomingCall])
+
   return (
     <div>
         <h1>Room</h1>
@@ -43,6 +86,15 @@ useEffect(() => {
             <h4>My Stream</h4>
             {/* <ReactPlayer muted playing height={100} width={200} url={myStream}/> */}
             <RoomVideo stream={myStream}/>
+            </>
+        )
+      }
+       {
+        remoteStream && (
+            <>
+            <h4>Remote Stream</h4>
+            {/* <ReactPlayer muted playing height={100} width={200} url={myStream}/> */}
+            <RoomVideo stream={remoteStream}/>
             </>
         )
       }
